94ce5be071e30147bdbc024595fe7d16875f22ee
Ankita Nath <145554790+anki2003ta@users.noreply.github.com>
2025-01-23 02:04:00 +0530

    Update Footer.jsx

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Footer = () => {
   return (
     <footer>
-        &#x2764; Designed And Developed By ELITE EDGE FITNESS
+        &#x2764; Designed And Developed By FitSphere
     </footer>
   )
 }
